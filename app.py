f670de6f7399dfdfcfb47a9d8e8bbf63ca7a52f8
LucaClementi <lucaclementi1d@gmai.com>
2025-12-03 21:54:14 +0100

    added top banner

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="FlashBet", layout="wide")
-st.title("⚡ FLASH BET ⚡ - Il sito per i pro del betting")
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    st.image("img/banner_white_code.png")
+
+
 
 # -----------------------------
 # LOGIN
